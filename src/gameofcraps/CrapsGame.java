7d13de498100bea8adb56add0fe2c72f038b6d5c
Francisco Ospina <francisco.ospina@gmail.com>
2017-11-29 12:13:24 -0500

    Implement CrapsGames
    Add tests for winning and losing and add logic for handling roll sequence.

--- src/gameofcraps/CrapsGame.java
+++ src/gameofcraps/CrapsGame.java
@@ -2,8 +2,11 @@ package gameofcraps;
 
 public class CrapsGame {
     private Dice pairOfDice;
-    private boolean win;
+    private boolean won;
+    private boolean lose;
     private int numRolls;
+    private int firstRoll;
+
 
     public CrapsGame(Dice customDice) {
         pairOfDice = customDice;
@@ -18,15 +21,39 @@ public class CrapsGame {
     }
 
     public void play() {
-        int diceResult = pairOfDice.roll();
-        win = diceResult == 7 || diceResult == 11;
+        pairOfDice.roll();
+        numRolls = 1;
+
+        int diceResult = pairOfDice.getSumOfFaces();
+        won = diceResult == 7 || diceResult == 11;
+        lose = diceResult == 2 || diceResult == 3 || diceResult == 12;
+
+        if (!won && !lose) {
+            firstRoll = diceResult;
+
+            do{
+                pairOfDice.roll();
+                numRolls ++;
+
+                diceResult = pairOfDice.getSumOfFaces();
+            }while(diceResult != 7 && diceResult != firstRoll);
+
+            won = diceResult == firstRoll;
+        }
+
     }
 
     public boolean getWin() {
-        return win;
+        return won;
+    }
+
+    public boolean getLose() {
+        return lose;
     }
 
     public int getNumRolls() {
         return -1;
     }
+
+
 }
--- test/gameofcraps/CrapsGameTest.java
+++ test/gameofcraps/CrapsGameTest.java
@@ -16,4 +16,50 @@ public class CrapsGameTest {
         assertTrue(game.getWin());
     }
 
+    @Test
+    public void winsIfComingOutRollIsEleven() {
+        CrapsGame game = new CrapsGame(new SequencedDice((11)));
+
+        game.play();
+
+        assertTrue(game.getWin());
+    }
+
+    @Test
+    public void winsIfNthRollIsEqualsToComingOutRoll() {
+        CrapsGame game = new CrapsGame(new SequencedDice(4, 5, 4));
+
+        game.play();
+
+        assertTrue(game.getWin());
+    }
+
+    @Test
+    public void lossesIfComingOutRollIsTwo() {
+        CrapsGame game = new CrapsGame(new SequencedDice((2)));
+
+        game.play();
+
+        assertTrue(game.getLose());
+    }
+
+    @Test
+    public void lossesIfComingOutRollIsThree() {
+        CrapsGame game = new CrapsGame(new SequencedDice((3)));
+
+        game.play();
+
+        assertTrue(game.getLose());
+    }
+
+    @Test
+    public void lossesIfComingOutRollIsTwelve() {
+        CrapsGame game = new CrapsGame(new SequencedDice((12)));
+
+        game.play();
+
+        assertTrue(game.getLose());
+    }
+
+
 }
