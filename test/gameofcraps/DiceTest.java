4b065064aedd05d5baa437af92916482c79840b1
Jorge Grajales <goonies2@hotmail.com>
2018-01-10 10:34:36 -0500

    Add DiceTest class

---
+++ test/gameofcraps/DiceTest.java
@@ -0,0 +1,21 @@
+package gameofcraps;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class DiceTest {
+
+    @Test
+    public void sumOfFacesIsWithinValidRange() {
+        Dice dice = new Dice();
+
+        for (int i=0; i<20; i++) {
+            dice.roll();
+            int roll = dice.getSumOfFaces();
+            assertThat(roll, allOf(greaterThanOrEqualTo(2), lessThanOrEqualTo(12)));
+        }
+
+
+    }
+}
